f755e814b719172b4ac199d48c4f8650f33f7df7
21x <114038240+0x21x@users.noreply.github.com>
2024-02-19 03:18:41 +0100

    Add script.sh

--- script.sh
+++ script.sh
@@ -1,1 +1,36 @@
-p
+#!/bin/bash
+
+# Install required dependencies
+sudo apt-get update
+sudo apt-get install -y curl
+
+# Install Proton CLI
+curl -sL https://repo.protonvpn.com/debian/dists/stable/main/binary-all/protonvpn-stable-release_1.0.3_all.deb -O /home/$USER/Downloads/protonvpn-stable-release_1.0.3_all.deb
+
+sudo apt install /home/$USER/Downloads/protonvpn-stable-release_1.0.3_all.deb
+
+sudo apt-get install protonvpn-cli
+
+# Clean up
+rm /home/$USER/Downloads/protonvpn-stable-release_1.0.3_all.deb
+
+# Verify installation
+proton-cli --version
+
+# Log in to ProtonVPN
+protonvpn-cli login
+
+# Connect to ProtonVPN
+protonvpn-cli connect
+
+# Configure kill switch and DNS leak protection
+protonvpn-cli ks --permanent
+protonvpn-cli dns --permanent
+
+# Enable kill switch and DNS leak protection
+protonvpn-cli ks --on
+protonvpn-cli dns --on
+
+# Verify connection
+protonvpn-cli status
+
