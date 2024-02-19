da2f6156262cc8698e0bc1607a124b64d2e6605d
21x <114038240+0x21x@users.noreply.github.com>
2024-02-19 03:32:47 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,7 +2,6 @@
 
 # Install required dependencies
 sudo apt-get update
-sudo apt-get install -y curl
 
 # Install Proton CLI
 curl -sL https://repo.protonvpn.com/debian/dists/stable/main/binary-all/protonvpn-stable-release_1.0.3_all.deb -O /home/$USER/Downloads/protonvpn-stable-release.deb
