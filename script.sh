979aca5454977513fb0de33400303fa86f1ee0ec
21x <114038240+0x21x@users.noreply.github.com>
2024-02-19 03:45:14 +0100

    Add files via upload

--- script.sh
+++ script.sh
@@ -2,6 +2,7 @@
 
 # Install required dependencies
 sudo apt-get update
+sudo apt-get install -y curl
 
 # Install Proton CLI
 curl -sL https://repo.protonvpn.com/debian/dists/stable/main/binary-all/protonvpn-stable-release_1.0.3_all.deb -O /home/$USER/Downloads/protonvpn-stable-release.deb
@@ -10,26 +11,27 @@ sudo apt install /home/$USER/Downloads/protonvpn-stable-release.deb
 
 sudo apt-get install protonvpn-cli
 
-# Clean up
-rm /home/$USER/Downloads/protonvpn-stable-release_1.0.3_all.deb
+
+# # Clean up
+# rm /home/$USER/Downloads/protonvpn-stable-release.deb
 
 # Verify installation
 proton-cli --version
 
 # Log in to ProtonVPN
-protonvpn-cli login
+# protonvpn-cli login
 
-# Connect to ProtonVPN
-protonvpn-cli connect
+# # Connect to ProtonVPN
+# protonvpn-cli connect
 
-# Configure kill switch and DNS leak protection
-protonvpn-cli ks --permanent
-protonvpn-cli dns --permanent
+# # Configure kill switch and DNS leak protection
+# protonvpn-cli ks --permanent
+# protonvpn-cli dns --permanent
 
-# Enable kill switch and DNS leak protection
-protonvpn-cli ks --on
-protonvpn-cli dns --on
+# # Enable kill switch and DNS leak protection
+# protonvpn-cli ks --on
+# protonvpn-cli dns --on
 
-# Verify connection
-protonvpn-cli status
+# # Verify connection
+# protonvpn-cli status
 
